4bfde029b535773bab49755a02c02900be4edb40
Enzo Jean <jnn4376a@ens.fsi>
2023-09-20 15:01:35 +0200

    1)

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -49,6 +49,10 @@ public class Gaulois {
 	}
 	
 	public static void main(String[] args) {
-		
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
+		System.out.println(asterix);
 	}
+
+
 }
